5a466bd4fb1c5ee2fec3ab0e16fb53f01e6964f4
Nahapet-Simonyan <nahapet.simonyan95@gmail.com>
2023-11-17 00:41:09 +0400

    delete
    delete

--- lib/config/amplitude/amplitude.dart
+++ lib/config/amplitude/amplitude.dart
@@ -1,10 +1,10 @@
-import 'package:amplitude_flutter/amplitude.dart';
-
-Future<void> initAmplitude() async {
-  final Amplitude analytics = Amplitude.getInstance(instanceName: "project");
-
-  analytics.init('e61cf86d2837ba893461a4c6ec1cec45');
-
-  analytics.logEvent('MyApp startup',
-      eventProperties: {'friend_num': 10, 'is_heavy_user': true});
-}
+// import 'package:amplitude_flutter/amplitude.dart';
+//
+// Future<void> initAmplitude() async {
+//   final Amplitude analytics = Amplitude.getInstance(instanceName: "project");
+//
+//   analytics.init('e61cf86d2837ba893461a4c6ec1cec45');
+//
+//   analytics.logEvent('MyApp startup',
+//       eventProperties: {'friend_num': 10, 'is_heavy_user': true});
+// }
